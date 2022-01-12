f87474eca6ae554c188817535cdd1bb3c27a1f4e
leonardoromero <leonardorommero@gmail.com>
2022-01-11 23:38:33 -0300

    tweak to quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -13,7 +13,7 @@ export const Quote = () => {
         <div className='flex justify-center bg-primary text-lg'>
             <blockquote className='p-4 italic text-neutral-700 w-10/12 md:w-max'>
                 <div className="quote-sign" aria-hidden="true">&ldquo;</div>
-                <p className='pl-5'>{fragments[random].verse}</p>
+                <p className='pl-5 z-10'>{fragments[random].verse}</p>
                 <cite className='text-right block text-neutral-600 hover:text-neutral-500'>
                     <a href={fragments[random].link} target='_blank' rel="noopener noreferrer">{fragments[random].song}</a>
                 </cite>
